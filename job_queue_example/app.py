f00834b8f18cff84c426b8d2509a5763a59e053a
Piotr Gretkierewicz <piotr.gretkierewicz@ncps.local>
2022-02-17 21:47:19 +0100

    Job_queue - moving Job creation out of the App class

--- job_queue_example/app.py
+++ job_queue_example/app.py
@@ -1,7 +1,6 @@
 import time
 
 from .events import Event, EventDistributionFactory
-from .jobs import Job
 
 
 class App:
@@ -15,7 +14,7 @@ class App:
         self.update_event: Event = Event()
         self.after_update_event: Event = Event()
 
-        self.job_list = [Job(f"Job nr {i}") for i in range(5)[::-1]]
+        self.job_list = []
 
     def log(self, message) -> None:
         print(f"Time: {time.time() - self._start_time:.2f}s | {message}")
--- job_queue_example/run_example.py
+++ job_queue_example/run_example.py
@@ -1,11 +1,13 @@
 """
 Module for running example of job queue
 """
+from .jobs import Job
 from .app import App
 from .entities import Manager, Worker
 
 if __name__ == "__main__":
     main_app = App()
+    main_app.job_list += [Job(f"Job nr {i}") for i in range(5)[::-1]]
 
     w1 = Worker(main_app, "Steve (w)")
     w2 = Worker(main_app, "Sophi (w)")
