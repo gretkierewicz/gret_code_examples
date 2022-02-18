5435bcc58887cdef840433d47ec78e1a9b0628f5
Piotr Gretkierewicz <piotr.gretkierewicz@ncps.local>
2022-02-18 13:08:09 +0100

    utils - conversion cleanup

--- code_sandbox/utils/tests/test_conversion.py
+++ code_sandbox/utils/tests/test_conversion.py
@@ -13,11 +13,11 @@ class TestTurnIntoFloats:
         assert _list == turn_into_floats(*_list)
 
     def test_turn_integer_range(self):
-        _list = [i for i in range(6)]
+        _list = list(range(6))
         assert _list == turn_into_floats(*_list)
 
     def test_turn_proper_string_range(self):
-        _list = [i for i in range(6)]
+        _list = list(range(6))
         assert _list == turn_into_floats(*[str(i) for i in _list])
 
     def test_wrong_string_passed(self):
